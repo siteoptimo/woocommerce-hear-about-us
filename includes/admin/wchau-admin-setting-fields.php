c34bbe981aeaba79adf7a8d9393fcca7b3a4e3cb
Pieter Carette <pieter@siteoptimo.com>
2015-01-12 09:08:48 +0100

    Value updates and default value for the source location select field.

--- includes/admin/wchau-admin-setting-fields.php
+++ includes/admin/wchau-admin-setting-fields.php
@@ -45,10 +45,12 @@ class WCHAU_Admin_Setting_Fields {
 			'desc_tip' => true,
 			'id'       => 'wchau_sourcelocation',
 			'type'     => 'select',
+			'class'    => 'chosen_select',
+			'default'  => 'profiles_and_orders',
 			'options'  => array(
-				__( 'both user profiles and orders', 'woocommerce-hear-about-us' ),
-				__( 'user profiles only', 'woocommerce-hear-about-us' ),
-				__( 'orders only', 'woocommerce-hear-about-us' )
+				'profiles_and_orders'           => __( 'both user profiles and orders', 'woocommerce-hear-about-us' ),
+				'profiles_only' => __( 'user profiles only', 'woocommerce-hear-about-us' ),
+				'orders_only'   => __( 'orders only', 'woocommerce-hear-about-us' )
 			)
 		);
 
@@ -56,23 +58,23 @@ class WCHAU_Admin_Setting_Fields {
 
 		$fields     = apply_filters( 'wchau_settings_fields', array(
 
-				array(
-					'title'    => __( 'Label', 'woocommerce-hear-about-us' ),
-					'desc'     => __( 'Customize the "where did you hear about us" label.', 'woocommerce-hear-about-us' ),
-					'id'       => 'wchau_label',
-					'type'     => 'text',
-					'default'  => __( 'Where did you hear about us?', 'woocommerce-hear-about-us' ),
-					'desc_tip' => true,
-				),
-				array(
-					'title'    => __( 'Possible answers', 'woocommerce-hear-about-us' ),
-					'desc'     => __( 'List all of the possible answers, one answer per line.', 'woocommerce-hear-about-us' ),
-					'id'       => 'wchau_options',
-					'type'     => 'textarea',
-					'default'  => implode( PHP_EOL, array( 'Google', 'Facebook', 'Twitter', 'A friend', 'Other' ) ),
-					'desc_tip' => true,
-				)
-			) );
+			array(
+				'title'    => __( 'Label', 'woocommerce-hear-about-us' ),
+				'desc'     => __( 'Customize the "where did you hear about us" label.', 'woocommerce-hear-about-us' ),
+				'id'       => 'wchau_label',
+				'type'     => 'text',
+				'default'  => __( 'Where did you hear about us?', 'woocommerce-hear-about-us' ),
+				'desc_tip' => true,
+			),
+			array(
+				'title'    => __( 'Possible answers', 'woocommerce-hear-about-us' ),
+				'desc'     => __( 'List all of the possible answers, one answer per line.', 'woocommerce-hear-about-us' ),
+				'id'       => 'wchau_options',
+				'type'     => 'textarea',
+				'default'  => implode( PHP_EOL, array( 'Google', 'Facebook', 'Twitter', 'A friend', 'Other' ) ),
+				'desc_tip' => true,
+			)
+		) );
 		$settings   = array_merge( $settings, $fields );
 		$sectionend = array( 'type' => 'sectionend', 'id' => 'wchau_sectionend' );
 
